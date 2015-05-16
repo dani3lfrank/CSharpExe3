bbb5a80bfe7b4d048f8ab652187e08d91d7e4528
Firman86 <firmar86@gmail.com>
2015-05-16 19:35:41 +0300

    update

---
+++ CSharpExe3/Ex03.GarageLogic/ElectricEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class ElectricEngine : Engine
+    {
+        public ElectricEngine(float i_PercentageOfEnergyLeft, float i_MaxEnergyCapacity) : base(i_PercentageOfEnergyLeft, i_MaxEnergyCapacity)
+        {
+        }
+
+        public override void FillEngine(float i_BatteryHoursToAdd, eEnergyType i_EnergyType)
+        {
+            m_MaxEnergyCapacity += (i_BatteryHoursToAdd / 60f);
+        }
+    }
+}
